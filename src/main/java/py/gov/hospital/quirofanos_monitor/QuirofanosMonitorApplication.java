3322e2cc9dbf52558ae81071c887d747649b8a22
Ivan-S93 <samudioivan93@gmail.com>
2026-01-07 14:08:14 -0300

    Proyecto base spring boot - monitoreo de quirofanos

---
+++ src/main/java/py/gov/hospital/quirofanos_monitor/QuirofanosMonitorApplication.java
@@ -0,0 +1,13 @@
+package py.gov.hospital.quirofanos_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuirofanosMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuirofanosMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/py/gov/hospital/quirofanos_monitor/QuirofanosMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package py.gov.hospital.quirofanos_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuirofanosMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
