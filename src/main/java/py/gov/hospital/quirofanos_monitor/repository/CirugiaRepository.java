906b37a78cf5774ecd8eacc33e99c16f7c49a2f4
Ivan-S93 <samudioivan93@gmail.com>
2026-01-09 14:12:31 -0300

    se actualizo CirugiaRepository y QuierofanoRepository  debido a que se uso enum para los estados

--- src/main/java/py/gov/hospital/quirofanos_monitor/repository/CirugiaRepository.java
+++ src/main/java/py/gov/hospital/quirofanos_monitor/repository/CirugiaRepository.java
@@ -2,10 +2,15 @@ package py.gov.hospital.quirofanos_monitor.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import py.gov.hospital.quirofanos_monitor.model.Cirugia;
+import py.gov.hospital.quirofanos_monitor.model.enums.EstadoCirugia; // Importa el Enum
 import java.util.Optional;
+import java.util.List;
 
 public interface CirugiaRepository extends JpaRepository<Cirugia, Long> {
-    //  Metodo perzonalizado para encontrar la cirugia activa en un quirofano especifico
-    // Este es importante para que pintar el cuadro en rojo en el frontend
-    Optional<Cirugia> findByQuirofanoIdAndEstado(Long quirofanoId, String estado);
+    
+    // Cambiamos String por EstadoCirugia
+    Optional<Cirugia> findByQuirofanoIdAndEstado(Long quirofanoId, EstadoCirugia estado);
+
+    // Tip Pro: Buscar todas las cirugías activas (útil para el monitor general)
+    List<Cirugia> findByEstado(EstadoCirugia estado);
 }
--- src/main/java/py/gov/hospital/quirofanos_monitor/repository/QuirofanoRepository.java
+++ src/main/java/py/gov/hospital/quirofanos_monitor/repository/QuirofanoRepository.java
@@ -1,7 +1,12 @@
 package py.gov.hospital.quirofanos_monitor.repository;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import py.gov.hospital.quirofanos_monitor.model.Quirofano;
+import py.gov.hospital.quirofanos_monitor.model.enums.EstadoQuirofano;
+import java.util.List;
 
 public interface QuirofanoRepository extends JpaRepository<Quirofano, Long> {
     
+    // Para mostrar solo quirófanos operativos en el monitor
+    List<Quirofano> findByEstadoNot(EstadoQuirofano estado);
 }
