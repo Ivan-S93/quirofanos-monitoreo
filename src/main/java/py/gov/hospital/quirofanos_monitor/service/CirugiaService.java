d617a2d801f20a1f77506011190b79b5dba37217
Ivan-S93 <samudioivan93@gmail.com>
2026-01-12 14:44:54 -0300

    se creo el archivo CirugiaService  para logica de estados de quirofano

---
+++ src/main/java/py/gov/hospital/quirofanos_monitor/service/CirugiaService.java
@@ -0,0 +1,45 @@
+package py.gov.hospital.quirofanos_monitor.service;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
+import py.gov.hospital.quirofanos_monitor.model.Cirugia;
+import py.gov.hospital.quirofanos_monitor.model.Quirofano;
+import py.gov.hospital.quirofanos_monitor.model.enums.EstadoCirugia;
+import py.gov.hospital.quirofanos_monitor.model.enums.EstadoQuirofano;
+import py.gov.hospital.quirofanos_monitor.repository.CirugiaRepository;
+import py.gov.hospital.quirofanos_monitor.repository.QuirofanoRepository;
+
+@Service
+public class CirugiaService {
+    private final CirugiaRepository cirugiaRepository; 
+    private final QuirofanoRepository quirofanoRepository;
+
+    public CirugiaService(CirugiaRepository cirugiaRepository, QuirofanoRepository quirofanoRepository) {
+        this.cirugiaRepository = cirugiaRepository;
+        this.quirofanoRepository = quirofanoRepository;
+    }
+
+    @Transactional
+    public Cirugia iniciarCirugia(Long quirofanoId, String descripcion, Integer duracion) {
+        Quirofano q = quirofanoRepository.findById(quirofanoId)
+                .orElseThrow(() -> new RuntimeException("Quirofano no encontrado"));
+        // Regla hospitalaria CLAVE
+        if (q.getEstado() == EstadoQuirofano.OCUPADO) {
+            throw new RuntimeException("Quirofano ocupado");
+        }
+
+        q.setEstado(EstadoQuirofano.OCUPADO);
+
+        Cirugia c = Cirugia.builder()
+                .descripcion(descripcion)
+                .horaInicio(LocalDateTime.now())
+                .duracionEstimada(duracion)
+                .estado(EstadoCirugia.EN_CURSO)
+                .quirofano(q)
+                .build();
+        return cirugiaRepository.save(c);
+    }
+}
